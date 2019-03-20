f20ee790eb409d738f41fbcec99e7b133a0b1286
DaianaVargas <dai.vgsg@gmail.com>
2019-03-20 09:08:27 -0300

    first version of comments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { throws } from 'assert';
 
 class App extends Component {
+  state = {
+    newComment: { content: '', date: '' },
+    comments: [
+      { content: "comentário 1", date: new Date() },
+      { content: "comentário 2", date: new Date() },
+      { content: "comentário 3", date: new Date() },
+    ]
+  }
+
+  sendComment = () => {
+    this.setState({
+      comments: [...this.state.comments, this.state.newComment],
+      newComment: ''
+    })
+  }
+
+  handleChange = event => {
+    this.setState({
+      newComment: { content: event.target.value, date: new Date() }
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* NewComments */}
+        <div>
+          <textarea value={this.state.newComment.content} onChange={this.handleChange}></textarea>
+          <button onClick={this.sendComment}>Enviar</button>
+        </div>
+        {/* Comments */}
+        <div>
+          {/* Comment */}
+          { this.state.comments.map( comment => {
+            return <div>
+                    <div>{comment.content}</div>
+                    <div>{comment.date.toLocaleString()}</div>
+                  </div>
+          })}
+        </div>
       </div>
     );
   }
