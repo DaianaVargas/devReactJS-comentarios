425409d55088521913dbe0a94d3138b546c8fd0d
DaianaVargas <dai.vgsg@gmail.com>
2019-04-01 08:29:56 -0300

    Creating functional stateless components

--- src/Comment.js
+++ src/Comment.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react'
-
-class Comment extends Component {
-    render(){
-        const { comment } = this.props
-        return (
-            <div>
-                {/* <div>{JSON.stringify(this.props.comment)}</div>  */}
-                
-                <div>Comentário: {comment.content}</div>
-                <div>Criação: {comment.date}</div>
-                <br/>
-            </div>
-        )
-    }
+import React from 'react'
+// pure-functions
+const Comment = ({comment}) => {
+    return (
+        <div>
+            {/* <div>{JSON.stringify(this.props.comment)}</div>  */}
+            
+            <div>Comentário: {comment.content}</div>
+            <div>Criação: {comment.date}</div>
+            <br/>
+        </div>
+    )
 }
 
 export default Comment
--- src/Comments.js
+++ src/Comments.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Comment from './Comment'
 
-class Comments extends Component {
-    render(){
-        const keys = Object.keys(this.props.comments)
-        return (
-            <div>
-                { keys.map( key => <Comment key={key} comment={this.props.comments[key]} /> )}
-            </div>
-        )
-    }
+const Comments = ({comments}) => {
+    const keys = Object.keys(comments)
+    return (
+        <div>
+            { keys.map( key => <Comment key={key} comment={comments[key]} /> )}
+        </div>
+    )
 }
 
 export default Comments
